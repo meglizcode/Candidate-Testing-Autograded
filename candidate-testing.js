0d0ea493b1826a643742ada3b39bc44fdd33200f
meglizcode <meglizjudd@gmail.com>
2024-03-29 14:38:53 -0500

    First Question

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,12 +3,12 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = "Name";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "Answer";
+let line = "\n_________________________\n"
 
 //TODO: Variables for Part 2
 let questions;
@@ -18,20 +18,26 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question('\nHello! Welcome to my quiz! \nPlease enter your name: ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+console.log('Question 1.\n\n' + question);
+candidateAnswer = input.question("Answer: ");
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
+if (candidateAnswer === correctAnswer) {
+  console.log("\nThat's Right! Sally Ride launched on June 18, 1983 aboard the Space Shuttle Challanger");
+} else if (candidateAnswer !== correctAnswer && candidateName === correctAnswer) {
+  console.log('\nWow "Sally" you really forgot you were the first in space?')
+} else {
+  console.log("\nSorry, that's not quite right but good try");
+}
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
@@ -42,7 +48,11 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log();
+if (candidateName === correctAnswer) {
+  console.log("\n\n\n...Oh you're messing with my aren't you. Ok SALLY let's get to it." + line)
+} else if (candidateName.length > 0) {
+   console.log('\n\n\nHello ' + candidateName + '!\nReady to get tested on some space trivia?' + line);
+} else console.log("\n\n\nRather remain anonymous yeah? Suit yourself. Ready for some space trivia?" + line)
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
