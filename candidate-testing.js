df2dc1968fe7a4f238a0dcf52708de27fd83e44d
meglizcode <meglizjudd@gmail.com>
2024-04-02 16:51:19 -0500

    Part2 Complete

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,11 +9,14 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "Answer";
 let line = "\n_________________________\n";
+let questionBreak = '---'
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+
+let questionNum = ["Question 1.","Question 2","Question 3.","Question 4.","Question 5"]
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -23,21 +26,29 @@ candidateName = input.question('\nHello! Welcome to my quiz! \nPlease enter your
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log('Question 1.\n\n' + question);
-candidateAnswer = input.question("Answer: ");
-
+ let i = 0;
+ while (i < questions.length) 
+ if (i < questions.length) {
+    console.log('\n' + questionNum[i] +'\n\n'+ questions[i]); 
+    candidateAnswers = input.question("Answer: ") + `\n`;
+    console.log(`${questionBreak}\nYour answer: ${candidateAnswers}Correct Answer: ${correctAnswers[i]}\n${questionBreak}`)
+    i++; 
+  } else
+  break
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if (candidateAnswer === correctAnswer) {
-  console.log("\nThat's Right! Sally Ride launched on June 18, 1983 aboard the Space Shuttle Challanger");
-} else if (candidateAnswer !== correctAnswer && candidateName === correctAnswer) {
-  console.log('\nWow "Sally" you really forgot you were the first in space?');
-} else {
-  console.log("\nSorry, that's not quite right but good try");
-}
+// if (candidateAnswer === correctAnswer) {
+//   console.log("\nThat's Right! Sally Ride launched on June 18, 1983 aboard the Space Shuttle Challanger");
+// } else if (candidateAnswer !== correctAnswer && candidateName === correctAnswer) {
+//   console.log('\nWow "Sally" you really forgot you were the first in space?');
+// } else {
+//   console.log("\nSorry, that's not quite right but good try");
+// }
+
+
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
