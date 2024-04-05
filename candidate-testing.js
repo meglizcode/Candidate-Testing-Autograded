53c10a587ee9fb8b01cc06f39a3bee53933141bf
meglizcode <meglizjudd@gmail.com>
2024-04-05 11:45:29 -0500

    Final Edit

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,65 +8,61 @@ let candidateName = "Name";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "Answer";
+
 let line = "\n_________________________\n";
-let questionBreak = '---'
+let questionBreak = '---';
 
 //TODO: Variables for Part 2
 
-let questionNum = ["Question 1.","Question 2","Question 3.","Question 4.","Question 5"]
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
 let candidateAnswers = [];
 
+let questionNum = ["1. ","2: ","3. ","4. ","5. "];
+
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-candidateName = input.question('\nHello! Welcome to my quiz! \nPlease enter your name: ');
+candidateName = input.question('\nWelcome to my quiz! \nPlease enter your name: ');
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
- let i = 0;
- while (i < questions.length) 
- if (i < questions.length) {
-    console.log('\n' + questionNum[i] +'\n\n'+ questions[i]); 
-    candidateAnswers = input.question("Answer: ") + `\n`;
-    console.log(`${questionBreak}\nYour answer: ${candidateAnswers}Correct Answer: ${correctAnswers[i]}\n${questionBreak}`)
-    i++; 
-  } else
-  break
-}
+ for (i = 0; i < questions.length; i++) 
+    candidateAnswers.push(input.question('\n' + questionNum[i] + questions[i] + `: `));
+};
 
 function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-// if (candidateAnswer === correctAnswer) {
-//   console.log("\nThat's Right! Sally Ride launched on June 18, 1983 aboard the Space Shuttle Challanger");
-// } else if (candidateAnswer !== correctAnswer && candidateName === correctAnswer) {
-//   console.log('\nWow "Sally" you really forgot you were the first in space?');
-// } else {
-//   console.log("\nSorry, that's not quite right but good try");
-// }
-
-
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
+  console.log(`\n${line}\nYour answers\n${candidateAnswers}\n\nCorrect Answers\n${correctAnswers}\n${questionBreak}`);
+  let grade;
+  let numCorrect = 0;
+  candidateAnswers = candidateAnswers.join().toUpperCase().split(',');
+  correctAnswers = correctAnswers.join().toUpperCase().split(',');
+  for (i = 0; i < questions.length; i++) {
+    if (candidateAnswers[i] === correctAnswers[i]) {
+    numCorrect++;
+    };
+  };
+grade = ((numCorrect / questions.length) * 100);
+console.log(`${numCorrect}/5 Final grade:${grade}%`);
+  if (grade > 70) {
+    console.log('Congradulations! You passed and are ready for take off!');
+  } else 
+    console.log(`Sorry, looks like you still need to check your engines, better luck next time.`);
   return grade;
-}
+};
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-if (candidateName === correctAnswer) {
-  console.log("\n\n\n...Oh you're messing with my aren't you. Ok SALLY let's get to it." + line);
-} else if (candidateName.length > 0) {
-   console.log('\n\n\nHello ' + candidateName + '!\nReady to get tested on some space trivia?' + line);
-} else console.log("\n\n\nRather remain anonymous yeah? Suit yourself. Ready for some space trivia?" + line);
+  if (candidateName === correctAnswer) {
+    console.log("\n\n...Oh you're messing with my aren't you. Ok SALLY let's get to it." + line);
+  } else if (candidateName.length > 0) {
+     console.log('\n\nHello ' + candidateName + '!\nReady to get tested on some space trivia?' + line);
+  } else console.log("\n\nRather remain anonymous yeah? Suit yourself. Ready for some space trivia?" + line);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
-}
+};
 
 // ----------- Don't write any code or change any code below this line ---------- //
 module.exports = {
